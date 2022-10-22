518e2dd39d08c3c5f38427eaed747eb640e63af9
srj42 <99497027+srj42@users.noreply.github.com>
2022-10-22 04:31:44 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,10 +7,11 @@ function openDialog() {
 const image_input = document.querySelector("#fileid");
 
 image_input.addEventListener("change", function() {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-      const uploaded_image = reader.result;
-      document.querySelector("#picture").style.backgroundImage = `url(${uploaded_image})`;
+        const reader = new FileReader();
+        reader.addEventListener("load", () => {
+        const uploaded_image = reader.result;
+        document.querySelector("#picture").style.backgroundImage = `url(${uploaded_image})`;
+        image.src = readerEvent.target.result;
     });
     reader.readAsDataURL(this.files[0]);
   });
